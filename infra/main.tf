e644d340e2d8a25a7d81fc74ad9d4fee204310f8
Matheus Rosa <matheus202rs@gmail.com>
2025-12-22 22:43:39 -0300

    fmt

--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,11 @@
 module "database" {
   source = "./database"
 
-  dynamodb_table_name          = var.dynamodb_table_name
-  dynamodb_billing_mode        = var.dynamodb_billing_mode
-  dynamodb_table_hash_key      = var.dynamodb_table_hash_key
-  dynamodb_table_attributes    = var.dynamodb_table_attributes
-#   dynamodb_gsi_name            = var.dynamodb_gsi_name
-#   dynamodb_gsi_hash_key        = var.dynamodb_gsi_hash_key
-#   dynamodb_gsi_projection_type = var.dynamodb_gsi_projection_type
+  dynamodb_table_name       = var.dynamodb_table_name
+  dynamodb_billing_mode     = var.dynamodb_billing_mode
+  dynamodb_table_hash_key   = var.dynamodb_table_hash_key
+  dynamodb_table_attributes = var.dynamodb_table_attributes
+  #   dynamodb_gsi_name            = var.dynamodb_gsi_name
+  #   dynamodb_gsi_hash_key        = var.dynamodb_gsi_hash_key
+  #   dynamodb_gsi_projection_type = var.dynamodb_gsi_projection_type
 }
