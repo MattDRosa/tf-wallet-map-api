1c52a8fdaea778e1bd0cd77ba6e164b393a1b898
Matheus Rosa <matheus202rs@gmail.com>
2025-12-22 21:37:21 -0300

    feat/fix: added missing gsi

--- infra/database/dynamodb.tf
+++ infra/database/dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "customer_wallets" {
+resource "aws_dynamodb_table" "customer_portfolios" {
   name         = var.dynamodb_table_name
   billing_mode = var.dynamodb_billing_mode
   hash_key     = var.dynamodb_table_hash_key
@@ -9,6 +9,12 @@ resource "aws_dynamodb_table" "customer_wallets" {
       name = attribute.value.name
       type = attribute.value.type
     }
+  }
+
+  global_secondary_index {
+    name            = var.dynamodb_gsi_name
+    hash_key        = var.dynamodb_gsi_hash_key
+    projection_type = var.dynamodb_gsi_projection_type
   }
 
 }
