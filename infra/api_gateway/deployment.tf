0c15e95e07249f21afcbc955924389e66be4645e
Matheus Rosa <matheus202rs@gmail.com>
2025-12-24 13:55:54 -0300

    recreating api gateway

--- infra/api_gateway/deployment.tf
+++ infra/api_gateway/deployment.tf
@@ -2,6 +2,7 @@ resource "aws_api_gateway_deployment" "wallet_map_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.wallet_map_api.id
 
   depends_on = [
-    aws_api_gateway_rest_api.wallet_map_api
+    aws_api_gateway_rest_api.wallet_map_api,
+    aws_api_gateway_method.head_example
   ]
 }
