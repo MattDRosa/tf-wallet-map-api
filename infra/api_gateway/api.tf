45244fa2d90724f0f0d1229b84fb738fa1e31168
Matheus Rosa <matheus202rs@gmail.com>
2025-12-23 22:35:59 -0300

    version set

--- infra/api_gateway/api.tf
+++ infra/api_gateway/api.tf
@@ -13,6 +13,10 @@ resource "aws_api_gateway_resource" "example" {
   rest_api_id = aws_api_gateway_rest_api.wallet_map_api.id
   parent_id   = aws_api_gateway_rest_api.wallet_map_api.root_resource_id
   path_part   = "example"
+  
+  lifecycle {
+    ignore_changes = [ all ]
+  }
 }
 
 resource "aws_api_gateway_method" "head_example" {
@@ -20,8 +24,4 @@ resource "aws_api_gateway_method" "head_example" {
   resource_id   = aws_api_gateway_resource.example.id
   http_method   = "HEAD"
   authorization = "NONE"
-
-  lifecycle {
-    ignore_changes = [ "all" ]
-  }
 }
